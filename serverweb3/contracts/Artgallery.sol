99bab18c29b3147b5b83a04acc7a373aaa490e34
Kowshik-18 <kowshikc1093@gmail.com>
2023-11-03 19:50:47 +0600

    Art block smart contracts, community creation , content publish, approval

--- serverweb3/contracts/Artgallery.sol
+++ serverweb3/contracts/Artgallery.sol
@@ -1,90 +1,93 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ArtblockToken is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply
-    ) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
+contract ArtBlock is ERC20 {
+    uint256 public constant Y = 1; // Y amount of ABX tokens
+    uint256 public constant Z = 5; // Z amount of community native tokens
+
+    struct Community {
+        string title;
+        string description;
+        uint256 tokenId;
     }
 
-    function buyTokens(uint256 etherAmount) public payable {
-        // Check that the correct amount of ether has been sent
-        require(msg.value == etherAmount, "Incorrect amount of ether sent");
-
-        // Calculate the amount of tokens to be minted
-        uint256 tokenAmount = etherAmount * 100; // Assuming 1 ether = 100 tokens
-
-        // Mint the tokens and assign them to the sender
-        _mint(msg.sender, tokenAmount);
-    }
-}
-
-contract Community {
-    ERC20 public artblockToken;
-    string public title;
-    string public description;
-    address public creator;
-    uint256 public tokenAmount;
-
-    constructor(
-        ERC20 _artblockToken,
-        string memory _title,
-        string memory _description,
-        uint256 _tokenAmount
-    ) {
-        artblockToken = _artblockToken;
-        title = _title;
-        description = _description;
-        creator = msg.sender;
-        tokenAmount = _tokenAmount;
-
-        // Approve the Community contract to transfer the ArtblockToken
-        artblockToken.approve(address(this), tokenAmount);
-
-        // Transfer the tokens from the creator to this contract and burn them
-        //artblockToken.transferFrom(msg.sender, address(this), tokenAmount);
+    struct Product {
+        string title;
+        string description;
+        uint256 tokenId;
+        uint256 stakeAmount;
+        bool isExclusive;
+        bool isApproved;
     }
 
-}
-
-contract CommunityToken is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply
-    ) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
-    }
-}
-
-contract TokenExchange {
-    ERC20 public communityToken;
-    ERC20 public abxToken;
-    uint256 public exchangeRate;
-
-    constructor(
-        ERC20 _communityToken,
-        ERC20 _abxToken,
-        uint256 _exchangeRate
-    ) {
-        communityToken = _communityToken;
-        abxToken = _abxToken;
-        exchangeRate = _exchangeRate;
+    struct Vote {
+        bool isUpvote;
+        uint256 weight;
     }
 
-    function exchangeTokens(uint256 tokenAmount) public {
-        // Transfer the community tokens from the sender to this contract
-        communityToken.transferFrom(msg.sender, address(this), tokenAmount);
+    mapping(address => Community[]) public communities;
+    mapping(address => Product[]) public products;
+    mapping(address => mapping(uint256 => Vote[])) public votes;
 
-        // Calculate the amount of ABX tokens to be transferred
-        uint256 abxAmount = tokenAmount * exchangeRate;
+    constructor() ERC20("ArtBlock", "ABX") {}
 
-        // Transfer the ABX tokens from this contract to the sender
-        abxToken.transfer(msg.sender, abxAmount);
+    function buyTokens(uint256 amount) public payable {
+        require(msg.value == amount * 1 ether, "Incorrect Ether value");
+        _mint(msg.sender, amount);
+    }
+
+    function createCommunity(string memory title, string memory description)
+        public
+    {
+        require(balanceOf(msg.sender) >= Y, "Not enough ABX tokens");
+        _burn(msg.sender, Y);
+        uint256 tokenId = totalSupply();
+        _mint(msg.sender, tokenId);
+        communities[msg.sender].push(Community(title, description, tokenId));
+    }
+
+    function publishProduct(
+        string memory title,
+        string memory description,
+        bool isExclusive
+    ) public {
+        require(
+            balanceOf(msg.sender) >= Z,
+            "Not enough community native tokens"
+        );
+        _burn(msg.sender, Z);
+        uint256 tokenId = totalSupply();
+        _mint(msg.sender, tokenId);
+        products[msg.sender].push(
+            Product(title, description, tokenId, Z, isExclusive, false)
+        );
+    }
+
+    function voteOnProduct(
+        address creator,
+        uint256 productId,
+        bool isUpvote
+    ) public {
+        uint256 weight = balanceOf(msg.sender);
+        votes[creator][productId].push(Vote(isUpvote, weight));
+    }
+
+    function finalizeVotes(address creator, uint256 productId) public {
+        uint256 totalWeight = 0;
+        uint256 upvoteWeight = 0;
+        for (uint256 i = 0; i < votes[creator][productId].length; i++) {
+            totalWeight += votes[creator][productId][i].weight;
+            if (votes[creator][productId][i].isUpvote) {
+                upvoteWeight += votes[creator][productId][i].weight;
+            }
+        }
+        if (upvoteWeight > totalWeight / 2) {
+            products[creator][productId].isApproved = true;
+            // Mint NFT
+        } else {
+            // Transfer staked tokens to community reserve
+        }
     }
 }
